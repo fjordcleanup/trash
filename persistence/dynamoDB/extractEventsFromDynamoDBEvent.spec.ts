d4fe6f83cd8e2f62afbccca636cfe4dc23636d63
Markus Tacker <Markus.Tacker@NordicSemi.no>
2025-08-04 14:51:02 +0200

    test: use domain event example

--- persistence/dynamoDB/extractEventsFromDynamoDBEvent.spec.ts
+++ persistence/dynamoDB/extractEventsFromDynamoDBEvent.spec.ts
@@ -8,78 +8,100 @@ void describe('extractEventsFromDynamoDBEvent()', () => {
 			extractEventsFromDynamoDBEvent({
 				Records: [
 					{
-						eventID: '2e8c3350216aa57b623003adb62133d1',
+						eventID: '4613b0adf71aa7f4602fcccfcba70e4c',
 						eventName: 'INSERT',
 						eventVersion: '1.1',
 						eventSource: 'aws:dynamodb',
-						awsRegion: 'eu-central-1',
+						awsRegion: 'eu-north-1',
 						dynamodb: {
-							ApproximateCreationDateTime: 1753035962,
+							ApproximateCreationDateTime: 1754259181,
 							Keys: {
 								eventId: {
-									S: '01K0MHBYFJT74K0NHCR72V4265',
+									S: '01K1RZXKEYCYQ5MSEQJ4Z5JNDE',
 								},
 								aggregateId: {
-									S: '928d0b1d-cab0-4d08-b565-4bdd941eeb7b',
+									S: '01K1RZXKEWDX1ESHFJVDHDWM2E',
 								},
 							},
 							NewImage: {
-								REVERSE_GEO: {
-									N: '1011',
-								},
-								GROUND_FIX: {
-									N: '123',
-								},
 								eventId: {
-									S: '01K0MHBYFJT74K0NHCR72V4265',
+									S: '01K1RZXKEYCYQ5MSEQJ4Z5JNDE',
 								},
 								aggregateId: {
-									S: '928d0b1d-cab0-4d08-b565-4bdd941eeb7b',
+									S: '01K1RZXKEWDX1ESHFJVDHDWM2E',
 								},
 								actorId: {
-									S: 'actor-2bf6766b-27be-4c65-8b55-d7e6f0d23348',
-								},
-								AGPS: {
-									N: '456',
+									S: 'https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_I8tzxRuIa:805c098c-4061-7008-104e-3cd6d870a7ab',
 								},
 								aggregateName: {
-									S: 'Team',
+									S: 'Report',
+								},
+								description: {
+									S: "There is an aluminum ship mast on the ground here. It's at least 6 meters long.",
 								},
 								eventName: {
-									S: 'ProxyUsageConfigured',
+									S: 'ReportCreated',
+								},
+								location: {
+									M: {
+										lng: {
+											N: '10.671264841123048',
+										},
+										lat: {
+											N: '59.917740637491505',
+										},
+									},
+								},
+								type: {
+									L: [
+										{
+											S: 'bulk',
+										},
+									],
 								},
 								eventTs: {
-									S: '2025-07-20T18:26:02.866Z',
+									S: '2025-08-03T22:13:01.022Z',
 								},
-								PGPS: {
-									N: '789',
+								photos: {
+									M: {
+										'photo-1.jpeg': {
+											NULL: true,
+										},
+									},
 								},
 								aggregateVersion: {
-									N: '3',
+									N: '1',
 								},
 							},
-							SequenceNumber: '28856700000818875059017386',
-							SizeBytes: 345,
+							SequenceNumber: '13999900001232806120223146',
+							SizeBytes: 503,
 							StreamViewType: 'NEW_IMAGE',
 						},
 						eventSourceARN:
-							'arn:aws:dynamodb:eu-central-1:529088254840:table/plans-and-pricing-persistence-eventsTable/stream/2025-07-14T14:22:52.405',
+							'arn:aws:dynamodb:eu-north-1:322276017280:table/fjordcleanup-trash-persistence-eventsTable/stream/2025-07-31T22:21:46.510',
 					},
 				],
 			}),
 			[
 				{
-					REVERSE_GEO: 1011,
-					GROUND_FIX: 123,
-					eventId: '01K0MHBYFJT74K0NHCR72V4265',
-					aggregateId: '928d0b1d-cab0-4d08-b565-4bdd941eeb7b',
-					actorId: 'actor-2bf6766b-27be-4c65-8b55-d7e6f0d23348',
-					AGPS: 456,
-					aggregateName: 'Team',
-					eventName: 'ProxyUsageConfigured',
-					eventTs: '2025-07-20T18:26:02.866Z',
-					PGPS: 789,
-					aggregateVersion: 3,
+					actorId:
+						'https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_I8tzxRuIa:805c098c-4061-7008-104e-3cd6d870a7ab',
+					aggregateId: '01K1RZXKEWDX1ESHFJVDHDWM2E',
+					aggregateName: 'Report',
+					aggregateVersion: 1,
+					description:
+						"There is an aluminum ship mast on the ground here. It's at least 6 meters long.",
+					eventId: '01K1RZXKEYCYQ5MSEQJ4Z5JNDE',
+					eventName: 'ReportCreated',
+					eventTs: '2025-08-03T22:13:01.022Z',
+					location: {
+						lng: 10.671264841123048,
+						lat: 59.917740637491505,
+					},
+					type: ['bulk'],
+					photos: {
+						'photo-1.jpeg': null,
+					},
 				},
 			],
 		)
