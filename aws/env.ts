ebcc2042e64d9680e36c917d2448b2e2135af579
Markus Tacker <Markus.Tacker@NordicSemi.no>
2025-09-07 14:49:58 +0200

    fix: this is not supported in Node.js it seems

--- aws/env.ts
+++ aws/env.ts
@@ -1,10 +1,10 @@
 import { GetCallerIdentityCommand, STSClient } from '@aws-sdk/client-sts'
 import type { Environment } from 'aws-cdk-lib'
 
-export const env = async (
-	sts: STSClient = new STSClient(),
-): Promise<Required<Environment>> => {
-	const { Account } = await sts.send(new GetCallerIdentityCommand({}))
+export const env = async (sts?: STSClient): Promise<Required<Environment>> => {
+	const { Account } = await (sts ?? new STSClient()).send(
+		new GetCallerIdentityCommand({}),
+	)
 	if (Account === undefined) throw new Error(`Failed to get caller identity!`)
 	return {
 		account: Account,
